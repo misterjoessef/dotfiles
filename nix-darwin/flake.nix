0ebc500ded2017e2a12b4f1893700c551671faad
misterjoessef <107667789+misterjoessef@users.noreply.github.com>
2025-05-09 12:26:20 -0700

    Added act, and updated aerospace

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -29,6 +29,7 @@
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
           environment.systemPackages = [
+            pkgs.act
             pkgs.aerospace
             pkgs.awscli2
             pkgs.bun
